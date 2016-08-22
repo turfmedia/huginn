ef6a937038c4da3e84a92fd5c17f5f097532053a
Alexey Volobuev <17valexl@gmail.com>
2016-08-22 09:02:49 +0300

    Update publisher_tasks. Now it available for dry run

--- app/models/agents/publisher_tasks.rb
+++ app/models/agents/publisher_tasks.rb
@@ -1,14 +1,18 @@
 module Agents
   class PublisherTasks < Agent
     # docs for how to create own Agent - https://github.com/cantino/huginn/wiki/Creating-a-new-agent
-    cannot_be_scheduled!
+    can_dry_run! #it gives run agent manually via /agents/:agent_id/dry_runs
+    default_schedule "never"
 
     description <<-MD
       The PublisherAgent creates an event for sending emails with La Gazette Turf to users.
     MD
 
     def default_options
-      { date: Date.tomorrow }
+      { 
+        date: '',
+        package_type: '',
+      }
     end
 
     # 
@@ -26,14 +30,21 @@ module Agents
     def receive(incoming_events)
       event = incoming_events.first
       return if event.blank? 
-      return if Event.where(agent_id: event.agent_id).select {|e| e.payload[:date] == event.payload[:date] }.map(&:payload).uniq.count < 2
-      date = Date.tomorrow.to_s
+      return if (not dry_run?) && Event.where(agent_id: event.agent_id).select do |e| 
+        begin
+          e.payload[:date].to_date == event.payload[:date].to_date 
+        rescue Exception => e
+          false
+        end
+      end.map(&:payload).uniq.count < 2
+
+      date = event.payload[:date] || Date.tomorrow.to_s
       gazette = Orchestrator::Tasks::Pipelines::Gazette.new(date)
 
       if gazette.launch!
         create_event payload: { date: date, status: "ok", pdf_link: gazette.link_to_pdf }
       else
-        create_event payload: { date: date, status: "failure" }
+        event = create_event payload: { date: date, status: "failure" }
       end
     end
 
